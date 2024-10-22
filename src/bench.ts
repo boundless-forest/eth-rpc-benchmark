ad050fd6e6303ad7a6d193442e4585437ff05716
bear <boundless.forest@outlook.com>
2024-10-22 16:44:57 +0800

    Finish `eth-blockNumber`

---
+++ src/bench.ts
@@ -0,0 +1,89 @@
+import { mkdir, writeFile, readFile, access } from "fs/promises";
+import { join } from "path";
+import { parse } from "smol-toml";
+
+export interface Config {
+	rpcProvider: string;
+	concurrency: number;
+	duration: number;
+	output: string;
+}
+
+export async function loadConfig(): Promise<Config> {
+	const tomlContent = await readFile("config.toml", "utf-8");
+	const config = parse(tomlContent);
+
+	// validate the config object
+	if (
+		typeof config.rpcProvider !== "string" ||
+		typeof config.concurrency !== "number" ||
+		typeof config.duration !== "number" ||
+		typeof config.output !== "string"
+	) {
+		throw new Error("Invalid configuration.");
+	}
+
+	return {
+		rpcProvider: config.rpcProvider,
+		concurrency: config.concurrency,
+		duration: config.duration,
+		output: config.output,
+	};
+}
+
+export interface BenchmarkResult {
+	method: string;
+	totalRequests: number;
+	elapsedTime: number;
+	avgRps: number;
+}
+
+export function benchStartConsole(method: string, config: Config) {
+	console.log(
+		`Starting benchmark ${method} to ${config.rpcProvider} with ${config.concurrency} concurrency for ${config.duration} milliseconds`
+	);
+}
+
+export function benchResultConsole(result: BenchmarkResult) {
+	console.log(
+		`Benchmark ${result.method} completed with ${result.totalRequests} requests in ${result.elapsedTime}ms (${result.avgRps} RPS)`
+	);
+}
+
+// Collect the benchmark result and save it to a JSON file, this is useful for comparing the performance of different RPC providers.
+export async function saveBenchMarkResult(
+	result: BenchmarkResult,
+	config: Config
+) {
+	interface JsonResult {
+		rpcProvider: string;
+		concurrency: number;
+		duration: number;
+		time: string
+		totalRequests: number;
+		avgRps: number;
+	}
+
+	const filePath = join(config.output, `${result.method}.json`);
+	let jsonResult: JsonResult[] = [];
+	try {
+		await access(filePath);
+	} catch (error) {
+		await mkdir(config.output, { recursive: true });
+		await writeFile(filePath, JSON.stringify(jsonResult));
+	}
+
+	// Append the new result to the existing JSON file
+	const json = await readFile(filePath, "utf-8");
+	jsonResult = JSON.parse(json);
+	jsonResult.push({
+		rpcProvider: config.rpcProvider,
+		concurrency: config.concurrency,
+		duration: config.duration,
+		time: new Date().toISOString(),
+		totalRequests: result.totalRequests,
+		avgRps: result.avgRps,
+	});
+	await writeFile(filePath, JSON.stringify(jsonResult, null, 2));
+	console.log(`Benchmark result saved to ${filePath}`);
+}
--- src/ethBlockNumber.ts
+++ src/ethBlockNumber.ts
@@ -1,17 +1,52 @@
-import { loadConfig, benchStartConsole } from "./run";
+import {
+	loadConfig,
+	benchStartConsole,
+	BenchmarkResult,
+	benchResultConsole,
+    saveBenchMarkResult,
+} from "./bench";
 import { ethers } from "ethers";
+import { performance } from "perf_hooks";
 
-function benchmark() {
-    const method = "eth_blockNumber";
-	const config = loadConfig();
-	const { rpcProvider, threads, duration } = config;
-
-    benchStartConsole(method, config);
-    
-	const provider = new ethers.JsonRpcProvider(rpcProvider);
-
-    // Create threads ts threads to request the eth_blockNumber for a specified duration.
-
+async function getBlockNumber(provider: ethers.JsonRpcProvider) {
+	return provider.getBlockNumber();
 }
 
-benchmark();
+async function runBenchmark() {
+	const method = "eth_blockNumber";
+	const config = await loadConfig();
+	const { rpcProvider, concurrency, duration } = config;
+	const provider = new ethers.JsonRpcProvider(rpcProvider);
+
+	benchStartConsole(method, config);
+
+	let startTime = performance.now();
+	let totalRequests = 0;
+
+	while (performance.now() - startTime < duration) {
+		const promises = Array(concurrency)
+			.fill(null)
+			.map(() => getBlockNumber(provider));
+
+		try {
+			await Promise.all(promises);
+			totalRequests += concurrency;
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	const elapsedTime = performance.now() - startTime;
+	const avgRps = totalRequests / (elapsedTime / 1000);
+	const result: BenchmarkResult = {
+		method,
+		totalRequests,
+		elapsedTime,
+		avgRps,
+	};
+
+	benchResultConsole(result);
+    saveBenchMarkResult(result, config);
+}
+
+await runBenchmark();
